f8795d4cfac9c1bd5ca93a243a899084be64793c
SQKAYEW <SQKAYEW@N4833.slake.sssb.com>
2021-11-17 10:01:50 +0800

    First Commit

---
+++ demo/src/main/java/com/example/demo/GreetingComponent.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class GreetingComponent {
+	public String getMessage() {
+		return "Hello from GreetingComponent";
+	}
+}
